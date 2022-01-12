b633292e9c8311e141be63c5cc2a605c2987ad40
Mathieu Suarez <msuarez001@iutbayonne.univ-pau.fr>
2022-01-12 18:17:32 +0100

    Création du générateur de données avec Faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -14,14 +14,101 @@ class AppFixtures extends Fixture
         //Création d'un générateur de données FAKER
         $faker = Faker\Factory::create('fr_FR');
 
+        /** CREATION DES FORMATIONS **/
 
-        $stage = new Stage();
-        $stage->setTitre($faker->realText($maxNbChars = 250, $inderSize=2 ));
-        $stage->addFormation("DUT Info");
-        $stage->setDescMission("Développement d'un erp servant à la recherche et au suivi des stages");
-        $stage->setEntreprise("AlphamediaBox");
+        $listeFormations = array();
+        $premierMot = array("Baccalauréat","Brevet","Diplôme","Doctorat","Master","Prepa");
+        $deuxiemeMot = array("Technologique","Professionnel","Scientifique","Universitaire","Informatique");
+        $troisiemeMot = array("de Programmation","de Danse","de Poterie","de BD","d'Art","de Dev Mobile","d'Escrime");
+
+        /* Génération des formations */
+
+        for($i=0;$i<20;$i++)
+        {
+            $premierTerme = $premierMot[$faker->numberBetween(0,count($premierMot)-1)];
+            $deuxiemeTerme = $deuxiemeMot[$faker->numberBetween(0,count($deuxiemeMot)-1)];
+            $troisiemeTerme = $troisiemeMot[$faker->numberBetween(0,count($troisiemeMot)-1)];
+
+        $formation = new Formation();
+        $formation->setLibelle($premierTerme." ".$deuxiemeTerme." ".$troisiemeTerme);
+        array_push($listeFormations,$formation);
+        $manager->persist($formation);
+
+        }
         
-        $manager->persist($stage);
-        $stage->flush();
+        /** CREATION DES ENTREPRISES **/
+
+        $listeActivites=array("Poterie",
+        "Programmation",
+        "Boulangerie",
+        "E-Sport",
+        "Developpement Web",
+        "Psychanalyse",
+        "Troubadour",
+        "Saltimbanque",
+        "Jongleur au chapeau au feu rouge",
+        "Samurai");
+
+        $listeEntreprises = array();
+
+        /* Génération des entreprises */
+        for($i=0;$i<10;$i++)
+        {
+            $nomEntreprise=$faker->company();
+
+            $entreprise=new Entreprise();
+            $entreprise->setNom($nomEntreprise." ".$faker->companySuffix());
+            $entreprise->setActivite($listeActivites[$faker->numberBetween(0,count($listeActivites)-1)]);
+            $entreprise->setAdresse($faker->address());
+            $entreprise->setEmailContact($nomEntreprise."@".$nomEntreprise.".com");
+            array_push($listeEntreprises,$entreprise);
+            $manager->persist($entreprise);
+        }
+
+        /** CREATION DES STAGES **/
+
+        /* Génération des stages */
+        for($i=0;$i<100;$i++)
+        {
+            $formationAjoutee=$listeFormations[$faker->numberBetween(0,count($listeFormations)-1)];
+            $stage = new Stage();
+            $stage->setTitre($faker->jobTitle());
+            $stage->setDescMission($faker->catchPhrase());
+            $stage->setEntreprise($listeEntreprises[$faker->numberBetween(0,count($listeEntreprises)-1)]);
+            $stage->addFormation($formationAjoutee);
+
+            /*Ajout de plusieurs formation pour un même stage*/
+            $nbDeFormations=$faker->numberBetween(0,count($listeFormations)-2);
+
+            $listeFormationsAjoutees=array();
+            for($j=0;$j<$nbDeFormations;$j++)
+            {
+                $estDejaAjoutee="false";
+                $formationAAjouter=$faker->numberBetween(0,count($listeFormations)-1);
+
+                //On vérifie si la formation n'a pas déjà été ajoutée
+                for($k=0;$k<count($listeFormationsAjoutees);$k++)
+                {
+                    if($listeFormationsAjoutees[$k]==$formationAAjouter)
+                    {
+                        $estDejaAjoutee="true";
+                    }
+                }
+                if($estDejaAjoutee=="false")
+                {
+                    array_push($listeFormationsAjoutees,$formationAAjouter);
+                    $stage->addFormation($listeFormations[$formationAAjouter]);
+                }
+                else
+                {
+                    /*On réduit le compteur du nombre de formations
+                    afin d'avoir le nombre souhaité car il est 
+                    incrémenté même quand rien n'est ajouté*/
+                    $j--;
+                }
+            }
+            $manager->persist($stage);
+        }
+        $manager->flush();
     }
 }
